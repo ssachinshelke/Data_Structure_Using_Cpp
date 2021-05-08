ad1bbef73911999cf689220af7278b59fc797573
Sachin Shelke <ssachinshelke@gmail.com>
2021-05-08 20:59:31 +0530

    Modularized the code

--- Templets/basic_templets.cpp
+++ Templets/basic_templets.cpp
@@ -35,99 +35,12 @@ Notes:
 */
 
 #include <iostream>
+#include "include/template.h"
+#include "include/sort.h"
 
 using namespace std;
 
-template <typename T>
-class Array {
-	T * ptr;
-	int size;
-public:
-	Array(T arr[], int s);
-	void print_arra();
-         ~Array() {
-		cout << "Destructor of Array called"<<endl;
-		delete [] this->ptr;
-		this->size = 0;
-	}
-	T * getPtr() {
-		return this->ptr;
-	}
 
-	int getSize() {
-		return this->size;
-	}
-
-	void bubble_sort();
-	void bubble_sort_with_recursion(T *arr, int n);
-};
-
-template <typename T>
-Array<T>::Array(T arr[], int size) {
-	cout << "Constructor of Array called"<<endl;
-	this->ptr = new T[size];
-	for(int i = 0; i < size; i++)
-		this->ptr[i] = *(arr + i);
-	this->size = size;
-}
-
-/*
-	Generic bubble sort for all type of datatypes.
-	Bubble sort implemented using templates.
-	Worst case and Avarege time complexity: O(n*n)
-	Best case time complexity: O(n)
-	Space complexity: O(1)
-*/
-template <typename T>
-void Array<T>::bubble_sort() {
-	int i, j;
-	T temp;
-
-	bool swapped = false;
-	for(i = 0; i < this->size - 1; i++) {
-		swapped = false;
-		for(j = 0; j < this->size - i - 1; j++) {
-			if(this->ptr[j] > this->ptr[j+1]) {
-				temp = this->ptr[j];
-				this->ptr[j] = this->ptr[j+1];
-				this->ptr[j+1] = temp;
-				swapped = true;
-			}
-		}
-		if(swapped == false) {
-			break;       /* This is optimized bubble sort 
-					as we stopped when we see array sorted */
-		}
-	}
-}
-
-template <typename T>
-void Array<T>::bubble_sort_with_recursion(T *arr, int n) {
-	int i;
-	T temp;
-
-	if(n == 1) {
-		cout << "Array Sorted using recursive bubble sort" << endl;
-		return; 
-	}
-
-	for(i = 0; i < n - 1; i++) {
-		if(arr[i] > arr[i+1]) {
-			temp = arr[i];
-			arr[i] = arr[i+1];
-			arr[i+1] = temp;
-		}
-	}
-
-	bubble_sort_with_recursion(this->ptr, n-1);
-}
-
-template <typename T>
-void Array<T>::print_arra() {
-	for(int i = 0; i < this->size; i++)
-		cout << this->ptr[i] <<" ";
-	cout << endl;
-}
 
 int main() {
 	int arr[6] = {8,2,7,4,5,6};
